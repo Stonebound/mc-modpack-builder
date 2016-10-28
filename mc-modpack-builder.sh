0d57572f9ff268590e8bf014ad79d146b8a79501
phit <phit@phit.link>
2016-10-28 18:47:47 +0200

    cleanup for future use

--- mc-modpack-builder.sh
+++ mc-modpack-builder.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# Minecraft Modpack builder from CurseVoice exports uploaded to Github
+# Minecraft Modpack Builder
+# Copyright (C) 2016 phit
 
 ############################
 ## Change these variables ##
 ############################
-packname=Principium
-gituser=Stonebound
-reponame=Principium
-branch="1.10.2"
-forgeversion="1.10.2-12.18.1.2026"
 
-## Extra variables
+packname=packname
+gituser=user
+reponame=packname
+branch="1.10.2"
+forgeversion="1.10.2-12.18.1.2086"
 fileRoot=$HOME/mcmodpackbuilder
+
+#############################
 
 # Display our output as bold for easy differentiation
 function boldDisplay
@@ -35,17 +37,21 @@ case "$1" in
         read packversion
 
         # Alternatively ask for Forge version instead of as a set variable above
-#        echo ""
-#        read forgeversion
+#       echo ""
+#       read forgeversion
 
         # Update from Github
         cd $reponame-$branch/
         git fetch --all
         git reset --hard origin/$branch
         git pull origin $branch
-
-        # Compressing files in proper Curse format
+        
+        ######################################################
+        ## Create Curse Client zip exluding extra git files ##
+        ######################################################
+        
         zip -r $fileRoot/builds/Curse_$packname-$packversion.zip * -x "mods/*" -x ".gitignore" -x "ISSUE_TEMPLATE.md" -x "README.md"
+        
         cd ..
 
         # Clear folder for pack with mods
@@ -56,60 +62,92 @@ case "$1" in
         cd $fileRoot/temp/withmods
         cp -R $fileRoot/temp/$reponame-$branch/mods .
         cp -R $fileRoot/temp/$reponame-$branch/overrides/config .
+        cp -R $fileRoot/temp/$reponame-$branch/overrides/resources .
 
-        # Create Technic Client Zip
-        # Download universal forge jar
-        # Zip up everything and exclude extra files
+        ####################################
+        ## Create Technic Client zip-file ##
+        ####################################
+        
+        # Download universal Forge jar, Technic still ships Forge witht he pack downloads
         mkdir bin
         wget -O bin/modpack.jar http://files.minecraftforge.net/maven/net/minecraftforge/forge/$forgeversion/forge-$forgeversion-universal.jar
+        
+        # Zip up everything
         zip -r $fileRoot/builds/Technic_$packname-$packversion.zip *
 
-        # Cleanup Technic files
+        # Cleanup extra Technic files
         rm -R bin
 
-        # Create Client zip
+        ####################################
+        ## Create vanilla client zip-file ##
+        ####################################
+        
+        # Download Forge installer
         wget -O forge-$forgeversion-installer.jar http://files.minecraftforge.net/maven/net/minecraftforge/forge/$forgeversion/forge-$forgeversion-installer.jar
+        
+        # Zip up everything and exclude extra files
         zip -r $fileRoot/builds/Client_$packname-$packversion.zip *
 
-        # Create MultiMC zip
+        #############################
+        ## Create MultiMC zip-file ##
+        #############################
+        
+        # Move our Forge installer for later use
         mv forge-$forgeversion-installer.jar ../
+        
+        # Copy MultiMC files from extras folder
         cp -R $fileRoot/extras/MultiMC/* .
         mkdir minecraft
+        
+        # Fix folder structure
         mv mods minecraft/mods
         mv config minecraft/config
+        mv resources minecraft/resources
+        
+        # Edit pack version
         sed -i -- 's/ [0-9].[0-9].[0-9]/ ${packversion}/g' instance.cfg
+
+        # Rename folder since that is where MultiMC gets the pack name from
         cd ..
         mv withmods "Principium ${packversion}"
+        
+        # Zip everything and rename folder back after it's done
         zip -r $fileRoot/builds/MultiMC_$packname-$packversion.zip "Principium ${packversion}"
         mv "Principium ${packversion}" withmods
+        
+        ############################
+        ## Create server zip-file ##
+        ############################
+        
+        # Undo MultiMC folder structure
         cd withmods
         mv minecraft/mods .
         mv minecraft/config .
+        mv minecraft/resources .
         rm -R minecraft
         rm -R patches
         rm .packignore
         rm instance.cfg
+        
+        # Get back the forge installer from earlier
         mv ../forge-$forgeversion-installer.jar .
 
-        # Create Server zip
+        # Use the installer to get the server files
         java -jar forge-*-installer.jar --installServer
         rm forge-*-installer.jar*
         mv forge-* forge_server.jar
+        
+        # Copy extra server files to folder
         cp $fileRoot/extras/ServerStart.* .
         cp $fileRoot/extras/eula.txt .
+        
         # Delete client side mods
-
         #################################
         ### ADD CLIENT SIDE MODS HERE ###
         #################################
         rm mods/ResourceLoader-*
-        rm mods/BetterFoliage*
-        rm mods/ChiselsBytes*
-        rm mods/itemscroller*
-        rm mods/MineMenu*
-        rm mods/moreoverlays*
-        rm mods/Neat*
 
+        # Zip up everything
         zip -r $fileRoot/builds/Server_$packname-$packversion.zip *
 
         # we are done!
@@ -119,7 +157,7 @@ case "$1" in
     setup)
         boldDisplay "Only run this once, this will wipe your current existing builds!"
 
-        # Wipe the file directory
+        # Wipe everything?
         read -p "Wipe your file directory now? [y/n]" -n 1 -r
         # Just quit out if they didn't say yes
         if ! [[ $REPLY =~ ^[Yy] ]]; then exit 0; fi
@@ -129,7 +167,7 @@ case "$1" in
             rm -rf $fileRoot
         fi
 
-        # Recreate directories
+        # Create directories
         mkdir $fileRoot
         cd $fileRoot
         mkdir -p {temp,builds,extras}
@@ -137,19 +175,30 @@ case "$1" in
         # Clone Repo from Github
         cd temp
         git clone -b $branch https://github.com/Stonebound/$reponame.git $reponame-$branch
-        
+
         ## Download extra files
-        cd extras
-        
+        ## You probably want to customize these, especially the MultiMC patches
+        cd $fileRoot/extras
+        wget https://raw.githubusercontent.com/Stonebound/mc-modpack-builder/master/files/ServerStart.sh
+        wget https://raw.githubusercontent.com/Stonebound/mc-modpack-builder/master/files/ServerStart.bat
+        wget https://raw.githubusercontent.com/Stonebound/mc-modpack-builder/master/files/eula.txt
+        mkdir MultiMC
+        cd MultiMC
+        mkdir -p {minecraft,patches}
+        wget https://raw.githubusercontent.com/Stonebound/mc-modpack-builder/master/files/MultiMC/instance.cfg
+        wget https://raw.githubusercontent.com/Stonebound/mc-modpack-builder/master/files/MultiMC/.packignore
+        cd patches
+        wget https://raw.githubusercontent.com/Stonebound/mc-modpack-builder/master/files/MultiMC/patches/net.minecraftforge.json
 
         # done
+        cd $fileRoot
         echo #newline for cleanliness
         boldDisplay "Setup complete."
         exit 0
     ;;
     *)
         boldDisplay "Minecraft Modpack Builder"
-        echo "Version 0.18 - Author phit <phit@hush.com>"
+        echo "Author phit <phit@hush.com>"
         echo #newline for cleanliness
         echo "Available Options:"
         echo "setup     Create directories and download required files"
